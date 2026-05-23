e1c2cff05fd5277e95718a390312e3a2d3e120c2
kn5225 <kanav.nagpal@hotmail.com>
2026-05-23 14:42:29 -0400

    added plugin

---
+++ plugin/deadweight.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: Deadweight
+ * Plugin URI: https://github.com/yourusername/deadweight
+ * Description: AI-powered audit of your money and time commitments
+ * Version: 0.1.0
+ * Author: Kanav Nagpal
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+define('DEADWEIGHT_VERSION', '0.1.0');
+define('DEADWEIGHT_PATH', plugin_dir_path(__FILE__));
+define('DEADWEIGHT_URL', plugin_dir_url(__FILE__));
