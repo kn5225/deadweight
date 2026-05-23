cd908c278e7cedbed20e06fd028047259839b500
kn5225 <kanav.nagpal@hotmail.com>
2026-05-23 14:50:47 -0400

    created plugin structure and registered plugin pages

--- plugin/deadweight.php
+++ plugin/deadweight.php
@@ -14,3 +14,28 @@ if (!defined('ABSPATH')) {
 define('DEADWEIGHT_VERSION', '0.1.0');
 define('DEADWEIGHT_PATH', plugin_dir_path(__FILE__));
 define('DEADWEIGHT_URL', plugin_dir_url(__FILE__));
+
+function deadweight_register_pages() {
+    add_shortcode('deadweight_onboarding', 'deadweight_render_onboarding');
+    add_shortcode('deadweight_dashboard', 'deadweight_render_dashboard');
+    add_shortcode('deadweight_audit', 'deadweight_render_audit');
+}
+add_action('init', 'deadweight_register_pages');
+
+function deadweight_render_onboarding() {
+    ob_start();
+    include DEADWEIGHT_PATH . 'templates/onboarding.php';
+    return ob_get_clean();
+}
+
+function deadweight_render_dashboard() {
+    ob_start();
+    include DEADWEIGHT_PATH . 'templates/dashboard.php';
+    return ob_get_clean();
+}
+
+function deadweight_render_audit() {
+    ob_start();
+    include DEADWEIGHT_PATH . 'templates/audit.php';
+    return ob_get_clean();
+}
